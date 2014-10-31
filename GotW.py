e567215550880cd7a54af9b5478eaf698bc44994
Phil S. Stein <phil.s.stein@gmail.com>
2014-10-31 14:29:08 -0400

    seems to be working

---
+++ GotW.py
@@ -0,0 +1,140 @@
+import logging
+from datetime import date
+import re
+
+from boardgamegeek import BoardGameGeek as BGG
+
+log = logging.getLogger('gotw')
+
+def getGotWPostText(game_name, next_game_name):
+    '''Take the name of a game, and return the GotW text to post to Reddit'''
+    bgg = BGG()
+    try:
+        game = bgg.game(game_name)
+    except boardgamegeek.exceptions.BoardGameGeekError as e:
+        log.critical(u'Error getting info from BGG on {}: {}'.format(
+            game_name, e))
+        return None
+
+    if not game:
+        log.critical(u'Unable to find {} on BGG'.format(game_name))
+        return None
+
+    text = u'[//]: # (GOTWS)\n'
+    text += (u'This week\'s game is [**{}**](http:{})\n\n'.format(game.name, game.image))
+    text += u' * **BGG Link**: [{}](http://www.boardgamegeek.com/boardgame/{})\n'.format(
+        game.name, game.id)
+
+    designers = getattr(game, u'designers', [u'Unknown'])
+    plural = u's' if len(designers) > 1 else u''
+    text += u' * **Designer{}**: {}\n'.format(plural, ', '.join(designers))
+    
+    publishers = getattr(game, u'publishers', [u'Unknown'])
+    plural = u's' if len(publishers) > 1 else u''
+    text += u' * **Publisher{}**: {}\n'.format(plural, ', '.join(publishers))
+    
+    text += u' * **Year Released**: {}\n'.format(game.year)
+    
+    mechanics = getattr(game, u'mechanics', [u'Unknown'])
+    plural = u's' if len(mechanics) > 1 else u''
+    text += u' * **Mechanic{}**: {}\n'.format(plural, ', '.join(mechanics))
+    
+    if game.min_players == game.max_players:
+        players = '{}'.format(game.min_players)
+    else:
+        players = '{} - {}'.format(game.min_players, game.max_players)
+    text += u' * **Number of Players**: {}\n'.format(players)
+    
+    text += u' * **Playing Time**: {} minutes\n'.format(game.playing_time)
+    
+    expansions = getattr(game, 'expansions', None)
+    if expansions:
+        text += u' * **Expansions**: {}\n'.format(', '.join([e.name for e in expansions]))
+
+    text += u' * **Ratings**:\n'
+    people = u'people' if game.users_rated > 1 else u'person'
+    text += u'    * Average rating is {} (rated by {} {})\n'.format(
+        game.rating_average, game.users_rated, people)
+    ranks = u', '.join([u'{}: {}'.format(
+        r[u'friendlyname'], r[u'value']) for r in game.ranks])
+    text += u'    * {}\n'.format(ranks)
+
+    text += u'\n\n'
+
+    text += u'**Description from Boardgamegeek**:\n\n{}\n\n'.format(game.description)
+
+    text += u'[//]: # (GOTWE)\n\n'
+    text += '---------------------\n\n'
+
+    if not next_game_name:
+        text += 'There is no Game of the Week scheduled for next week.'
+    else:
+        try:
+            game = bgg.game(next_game_name)
+        except boardgamegeek.exceptions.BoardGameGeekError as e:
+            log.critical(u'Error getting info from BGG on {}: {}'.format(
+                next_game_name, e))
+
+        if not game:
+            text += 'Next Week: {}'.format(next_game_name)
+        else:
+            text += ('Next Week: [**{}**](http://www.boardgamegeek'
+                     '.com/boardgame/{})\n\n'.format(game.name, game.id))
+
+    return text
+
+def updateGotWWiki(page, gotws, post_id):
+    ### update the wiki page.
+    # remove the gotw from the calendar
+    search_for = '\[//]:\s\(CALS\)\s.+\[//]:\s\(CALE\)\s'
+    new_cal_list = u''.join([u' * {}\n'.format(g) for g in gotws[1:]])
+    new_cal = '[//]: (CALS)\n' + new_cal_list + '[//]: (CALE)\n'
+    new_wiki_page = re.sub(search_for, new_cal, page, flags=re.DOTALL)
+
+    if new_wiki_page == page:
+        log.error('Error updating the calenda on the wiki page.')
+        return None
+
+    # load up the archive, add this week's gotw, sort by date, subs in the new list.
+    games = re.findall('(\d{4}-\d{2}-\d{2}) : \[([^\]]+)\]\(/(\w+)\)', new_wiki_page)
+    if not games:
+        log.critical('Unable to find archived GOTW links in wiki page')
+        return None
+
+    # add the new game to the archive list.
+    games.append(['{}'.format(date.today()), gotws[0], post_id])
+
+    # now write them out and subst into wiki page.
+    # (sort by name for reinsertion)
+    games = sorted(games, key=lambda x: x[1])
+    new_arch_list = u''.join([u' * {} : [{}](/{})\n'.format(g[0], g[1], g[2]) for g in games])
+    new_arch = '[//]: (GOTWS)\n' + new_arch_list + '[//]: (GOTWE)\n'
+    search_for = '\[//]:\s\(GOTWS\)\s.+\[//]:\s\(GOTWE\)\s'
+    new_wiki = re.sub(search_for, new_arch, new_wiki_page, flags=re.DOTALL)
+
+    return new_wiki
+
+def updateGotWSidebar(sidebar, game_name, post_id):
+    # update in two places. The linkbar and the sidebar. 
+    # link bar is not distinguished by a comment, but does have
+    # "[Game of the Week:..." which we can search for.
+    new_sb = re.sub('\[Game of the Week:[^\]]+\]\(/\w+\)\s', 
+                    '[Game of the Week: {}](/{}) '.format(game_name, post_id), 
+                    sidebar,
+                    flags=re.DOTALL)
+    
+    if new_sb == sidebar:
+        log.error('Error updating GOTW in linkbar.')
+        return sidebar
+
+    # sidebar link ends with " [//]: # (GOTWLINK)"
+    new_new_sb = re.sub('\[//\]: # \(GOTWLINK\)\n\[[^\]]+\]\(/\w+\)',
+                        '[//]: # (GOTWLINK)\n[{}](/{})'.format(game_name, post_id), 
+                        new_sb,
+                        flags=re.DOTALL)
+
+    if new_new_sb == new_sb:
+        log.error('Error updating GOTW in link in sidebar.')
+        return sidebar
+
+    return new_new_sb
---
+++ update_gotw.py
@@ -0,0 +1,88 @@
+import logging
+import argparse
+import re
+from sys import exit
+from html.parser import HTMLParser  # python 3.3 or older
+
+import praw
+
+from ArgParseLogging import addLoggingArgs, handleLoggingArgs
+from GotW import getGotWPostText, updateGotWWiki, updateGotWSidebar
+
+
+log = logging.getLogger('gotw')
+
+if __name__ == '__main__':
+    '''A simple script that posts the game of the week for /r/boardgames'''
+    ap = argparse.ArgumentParser()
+    ap.add_argument(u'-w', u'--wiki', help=u'The wiki page from which to read/write the calendar info') 
+    ap.add_argument(u'-s', u'--subreddit', help=u'The subreddit to update. Must have the gotw wiki '
+                    u'page.')
+    addLoggingArgs(ap)
+    args = ap.parse_args()
+    handleLoggingArgs(args)
+
+    wiki_path = args.wiki if args.wiki else u'game_of_the_week'
+    subreddit = args.subreddit if args.subreddit else u'phil_s_stein'
+
+    reddit = praw.Reddit(u'Game of the Week poster for /r/boardgames by /u/phil_s_stien')
+    reddit.login()  # use ambient praw.ini or stdin/getpass
+
+    gotw_wiki = reddit.get_wiki_page(subreddit, wiki_path)
+    log.debug(u'got wiki data: {}'.format(gotw_wiki.content_md))
+
+    # finding the next GOTW is done in two parts. Find the wiki chunk, then find the list 
+    # of games within that chunk
+    search_for = u'\[//]:\s\(CALS\)\s+\s+\*\s+.*\[//]:\s\(CALE\)'
+    match = re.search(search_for, gotw_wiki.content_md, flags=re.DOTALL)
+    if not match:
+        log.critical(u'Unable to find the upcoming GOTW in the wiki page "{}". Are there embedded'
+                     u' delimiters in the page [//]: (CALS) and [//]: (CALE)?'.format(wiki_path))
+        exit(1)
+
+    cal_games = re.findall(u'\*\s+(.*)\s+', match.group(0))
+    if not cal_games:
+        log.critical(u'There are no games of the week queued up. Nothing to do.')
+        exit(2)
+
+    cal_games = [g.rstrip('\r\n') for g in cal_games]
+    next_gotw_name = cal_games[1] if len(cal_games) > 2 else None
+    log.info(u'found next game of the week: {}. Followed by {}'.format(
+        cal_games[0], next_gotw_name))
+
+    # get the text of the GotW post.
+    post_text = getGotWPostText(cal_games[0], next_gotw_name)
+
+    if not post_text:
+        log.critical(u'Error getting GotW post text')
+        exit(3)
+
+    log.debug('Posting gotw text: {}'.format(post_text))
+
+    post = reddit.submit(subreddit, title='Game of the Week: {}'.format(cal_games[0]), text=post_text)
+    post.distinguish(as_made_by='mod')
+
+    if len(cal_games) <= 2:
+        # GTL - figure out ho to send modmail here and let mods 
+        # know there is no GOTW scheduled for next week.
+        pass
+
+    new_wiki_page = updateGotWWiki(gotw_wiki.content_md, cal_games, post.id)
+    if not new_wiki_page:
+        log.critical('Unable to update GotW wiki page for some reason.')
+        exit(4)
+
+    reddit.edit_wiki_page(subreddit=subreddit, page=wiki_path, content=new_wiki_page,
+                          reason=u'GotW post update for {}'.format(cal_games[0]))
+
+    # finally update the sidebar/link menu
+    sidebar = HTMLParser().unescape(reddit.get_subreddit(subreddit).get_settings()["description"])
+    new_sidebar = updateGotWSidebar(sidebar, cal_games[0], post.id)
+    if new_sidebar == sidebar:
+        log.critical('Error updating the sidebar for GotW.')
+        exit(5)
+
+    reddit.get_subreddit(subreddit).update_settings(description=new_sidebar)
+    log.info('Sidebar updated with new GotW information.')
+
+    exit(0)  # success!
